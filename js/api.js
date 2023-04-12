04cf2a6e0f1c82bc772e37d778638bc07d62ffe0
vcesarini <valeriacesarini@gmail.com>
2023-04-12 17:44:24 -0300

    api local

--- js/api.js
+++ js/api.js
@@ -1,25 +1,22 @@
-    
-    /* FAKE STORE */
+const fakeStore = document.querySelector('#store')
+//    const producto = fetch('https://fakestoreapi.com/products?limit=4')
+const producto = fetch('../json/productos.json')
 
-    const fakeStore = document.querySelector('#store')
-    const producto = fetch('../json/fake.json')
-
-    producto.then(res => res.json())
-            .then(data => {
-                for (const elemento of data) {
-                    const nuevoDiv = document.createElement('div');
-                    nuevoDiv.classList.add('card');
-                    nuevoDiv.innerHTML = `
-                        <img src="${elemento.image}" class="card-img-top" alt="${elemento.title}">
-                        ${id}
-                        <div class="card-body">
-                          <h5 class="card-title">${elemento.title}</h5>
-                          <p>${elemento.category}</p>
-                          <p class="card-text">${elemento.description}</p>
-                          <p>Precio: $${elemento.price}</p>
-                            <a href="#" class="btn btn-primary">COMPRAR</a>
-                        </div>
-                    `;
-                    fakeStore.appendChild(nuevoDiv)
-                }
-            });
+producto.then(res => res.json())
+        .then(data => {
+        for (const elemento of data) {
+            const nuevoDiv = document.createElement('div');
+            nuevoDiv.classList.add('card');
+            nuevoDiv.innerHTML = `
+                <img src="${elemento.img}" class="card-img-top" alt="${elemento.title}">
+                <div class="card-body">
+                    <h5 class="card-title">${elemento.titulo}</h5>
+                    <p>${elemento.categoria}</p>
+                    <p class="card-text">${elemento.descripcion}</p>
+                    <p>Precio: $${elemento.precio}</p>
+                    <a href="#" class="btn btn-primary text-uppercase">${elemento.boton}</a>
+                </div>
+            `;
+            fakeStore.appendChild(nuevoDiv)
+        }
+});
