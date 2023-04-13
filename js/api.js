800640a00f1081d5bce3442d3f06ed913f8a6f49
vcesarini <61749416+vcesarini@users.noreply.github.com>
2023-04-13 15:10:44 -0300

    Actualizar api.js

--- js/api.js
+++ js/api.js
@@ -22,7 +22,23 @@ producto.then(res => res.json())
 });
 
 document.addEventListener('DOMContentLoaded', traerProductos)
+const contenedor = document.querySelector('#contenedor')
 
 async function traerProductos(){
 const url = 'http://fakestoreapi.com/products'
+
+try {
+const resultado = await fetch(url)
+const respuesta = await resultado.json()
+pintarProductos(respuesta)
+} catch (error) {
+console.log(error)
+}
+}
+
+function pintarProductos(productos){
+productos.forEach((prod) => {
+const {id, title, price, category, description, image } = prod;
+contenedor.innerHTML += ``;
+});
 }
