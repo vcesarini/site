e8db66091686d0ccd9e7f2d0fc3f3cbb7a13609b
vcesarini <valeriacesarini@gmail.com>
2023-04-25 12:12:46 -0300

    carrito
    add carrito. youtuber

--- js/api.js
+++ js/api.js
@@ -1,56 +1,33 @@
-// const fakeStore = document.querySelector('#store')
-// //    const producto = fetch('https://fakestoreapi.com/products?limit=4')
-// const producto = fetch('../json/productos.json')
-
-// producto.then(res => res.json())
-//         .then(data => {
-//         for (const elemento of data) {
-//             const nuevoDiv = document.createElement('div');
-//             nuevoDiv.classList.add('card');
-//             nuevoDiv.innerHTML = `
-//                 <img src="${elemento.img}" class="card-img-top" alt="${elemento.title}">
-//                 <div class="card-body">
-//                     <h5 class="card-title">${elemento.titulo}</h5>
-//                     <p>${elemento.categoria}</p>
-//                     <p class="card-text">${elemento.descripcion}</p>
-//                     <p>Precio: $${elemento.precio}</p>
-//                     <a href="#" class="btn btn-primary text-uppercase">${elemento.boton}</a>
-//                 </div>
-//             `;
-//             fakeStore.appendChild(nuevoDiv)
-//         }
-// });
-
 document.addEventListener('DOMContentLoaded', traerProductos)
 const contenedor = document.querySelector('#contenedor')
-// const modal = new bootstrap.Modal('#modal',{})
 
 async function traerProductos(){
 const url = '../json/productos.json'
-//const url = 'https://fakestoreapi.com/products?limit=4'
+
 try {
-const resultado = await fetch(url)
-const respuesta = await resultado.json()
-pintarProductos(respuesta)
-} catch (error) {
-console.log(error)
-}
+        const resultado = await fetch(url)
+        const respuesta = await resultado.json()
+        pintarProductos(respuesta)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 function pintarProductos(productos){
-productos.forEach((prod) => {
-const {id, title, price, category, description, image, button } = prod;
-contenedor.innerHTML += `
-<div class="card">
-<img class="img-producto" src="${image}">
-<div class="card-body">
-<h5 class="card-title">${title}</h5>
-<h6>${category}</h6>
-<p class="control-texto">${description}</p>
-<p>Precio: $${price}</p>
-<button class="btn btn-primary">${button}</button>
-</div>
-</div>
-`;
-});
+    productos.forEach((prod) => {
+    const {sku, title, price, category, description, image, gramos, cc, button } = prod;
+    contenedor.innerHTML += `
+        <div class="card">
+        <h6>${sku}</h6>
+        <img class="img-producto" src="${image}">
+        <div class="card-body">
+            <h5 class="card-title">${title}${cc}${gramos}</h5>
+            <h6>${category}</h6>
+            <p class="control-texto">${description}</p>
+            <p>Precio: $${price}</p>
+            <button class="btn btn-primary">${button}</button>
+        </div>
+        </div>
+        `;
+    });
 }
---
+++ js/stock.js
@@ -0,0 +1,38 @@
+let stockProductos = [
+    {
+        tipo: "gaseosas",
+        id: 1,
+        nombre: "Coca Cola 2.5",
+        precio: 500,
+        cantidad: "",
+        img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/312/763/products/coca-cola-comun-225-lts_93109471-460c97fcff6110b91d15981366522182-640-0.jpg",
+        desc: "las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.",
+      },
+      {
+        tipo: "alimentos",
+        id: 2,
+        nombre: "Pan Lactal",
+        precio: 640,
+        cantidad: 125,
+        img: "https://carrefourar.vtexassets.com/arquivos/ids/308684/7793890258769_02.jpg?v=638145971063270000",
+        desc: "las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.",
+      },
+      {
+        tipo: "fiambreria",
+        id: 3,
+        nombre: "Mortadela Paladini",
+        precio: 300,
+        cantidad: 400,
+        img: "https://almacencooperativo.com.ar/wp-content/uploads/2020/11/Mortadelita-300gr-Paladini.jpg",
+        desc: "las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.",
+      },
+      {
+        tipo: "vinos",
+        id: 4,
+        nombre: "Viña de Balbo",
+        precio: 1500,
+        cantidad: 2,
+        img: "https://statics.dinoonline.com.ar/imagenes/full_600x600_ma/3060113_f.jpg",
+        desc: "las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.",
+      }
+]
